3ad3e039bea4878d1597550d4264d2cb53f0ea4c
Francois Martin <contact@francois-martin.com>
2024-12-27 22:25:05 +0100

    Updated dependencies

--- packages/front/src/data/constants.ts
+++ packages/front/src/data/constants.ts
@@ -1,6 +1,6 @@
 export const SITE = {
   lang: 'en',
   title: 'Astro HapiJS starter',
-  description: 'An application template based on Astro, VueJS, PrimeVue, TailwindCSS, HapiJS, Prisma and BetterAuth.',
+  description: 'An application template based on Astro, VueJS, PrimeVue, TailwindCSS, HapiJS and BetterAuth.',
   url: 'http://localhost',
 };
