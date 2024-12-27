b26afc068b98b87bb2396ba969ba905657c4804c
Francois Martin <contact@francois-martin.com>
2024-12-27 23:04:38 +0100

    Demo gif + social HTML tags

--- packages/front/src/components/landing/Hero.vue
+++ packages/front/src/components/landing/Hero.vue
@@ -2,6 +2,6 @@
   <h1>An easier way to manage <span class="text-primary">authentication</span></h1>
   <p class="pt-8 text-xl">Ready to use auth template based on Astro, VueJS, PrimeVue, TailwindCSS, HapiJS and BetterAuth.</p>
   <div class="pt-8">
-    <Skeleton width="100%" height="45vh" class="mx-auto"/>
+    <slot name="demo-gif"/>
   </div>
 </template>
